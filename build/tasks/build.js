c58bdb16777b965f7772d930e738af9b3ba5a2e4
Dan Elebash <danelebash@hotmail.com>
2015-09-28 07:11:01 -0400

    package update

--- build/tasks/build.js
+++ build/tasks/build.js
@@ -2,90 +2,80 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var runSequence = require('run-sequence');
 var changed = require('gulp-changed');
-//var plumber = require('gulp-plumber');
+var plumber = require('gulp-plumber');
 var ts = require('gulp-typescript');
 var merge = require('merge2');
 var sourcemaps = require('gulp-sourcemaps');
 var paths = require('../paths');
 var assign = Object.assign || require('object.assign');
-var autoprefixer = require('gulp-autoprefixer');
-var concat = require('gulp-concat');
-var minifycss = require('gulp-minify-css');
-var htmlmin = require('gulp-htmlmin');
-var imagemin = require('gulp-imagemin');
-var rename = require('gulp-rename');
+
 
 //using typescript
 gulp.task('build-system', function () {
-    var tsResult = gulp.src([paths.sourceTS, paths.typings, paths.aurelia], {base: paths.root})
-        //.pipe(changed(paths.output, {extension: '.ts'}))
-        .pipe(sourcemaps.init({loadMaps: true}))
-        .pipe(ts({
-            typescript: require('typescript'),
-            declaration: false,
-            noExternalResolve: true,
-            target: "es5",
-            module: "system",
-            emitDecoratorMetadata: true,
-            experimentalDecorators: true
-        }));
-    return merge([ // Merge the two output streams, so this task is finished when the IO of both operations are done.
-        tsResult.pipe(sourcemaps.write({includeContent: false, sourceRoot: '/src'})),
-        tsResult.dts.pipe(gulp.dest(paths.typingsOutput)),
-        tsResult.js.pipe(gulp.dest(paths.output))
-    ]);
+  var tsResult = gulp.src([paths.sourceTS, paths.typings, paths.aurelia], {base: paths.root})
+    //.pipe(changed(paths.output, {extension: '.ts'}))
+    .pipe(sourcemaps.init({loadMaps: true}))
+    .pipe(ts({
+      typescript: require('typescript'),
+      declarationFiles: false,
+      noExternalResolve: true,
+      target: "es5",
+      module: "commonjs",
+      emitDecoratorMetadata: true,
+      experimentalDecorators: true
+    }));
+  return merge([ // Merge the two output streams, so this task is finished when the IO of both operations are done.
+    tsResult.pipe(sourcemaps.write({includeContent: false, sourceRoot: '/src'})),
+    tsResult.dts.pipe(gulp.dest(paths.typingsOutput)),
+    tsResult.js.pipe(gulp.dest(paths.output))
+  ]);
 });
 
 gulp.task('build-sass', function () {
-    gulp.src(paths.sass + '**/styles.scss', {base: paths.sass})
-        //.pipe(changed(paths.sass, {extension: '.scss'}))
-        .pipe(sourcemaps.init())
-        .pipe(sass({
-            style: 'expanded',
-            //includePaths: [
-            //  paths.sass
-            //  //   paths.jspmDir + '/github/Dogfalo/materialize@0.96.0/sass',
-            //],
-            errLogToConsole: true
-        }))
-        .pipe(sourcemaps.write('./'))
-        .pipe(rename('sass.css'))
-        .pipe(gulp.dest(paths.css))
+  gulp.src(paths.sass + '**/styles.scss', {base: paths.sass})
+    //  .pipe(changed(paths.sass, {extension: '.scss'}))
+    .pipe(sourcemaps.init())
+    .pipe(sass({
+      style: 'expanded',
+      //includePaths: [
+      //  paths.sass
+      //  //   paths.jspmDir + '/github/Dogfalo/materialize@0.96.0/sass',
+      //],
+      errLogToConsole: true
+    }))
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest(paths.cssOutput))
 });
 
 // copies changed css files to the output directory
 gulp.task('build-css', function () {
-    return gulp.src(paths.css + '**/*.css', {base: paths.css})
-        // .pipe(changed(paths.cssOutput, {extension: '.css'}))
-        .pipe(autoprefixer({
-            browsers: ['last 2 versions'],
-            cascade: false
-        }))
-        .pipe(concat('all.css'))
-        .pipe(minifycss())
-        .pipe(rename('style.min.css'))
-        .pipe(gulp.dest(paths.cssOutput));
+  return gulp.src(paths.css)
+    .pipe(changed(paths.output, {extension: '.css'}))
+    .pipe(gulp.dest(paths.output));
 });
 
 // copies changed html files to the output directory
 gulp.task('build-html', function () {
-    return gulp.src(paths.html)
-        // .pipe(changed(paths.output, {extension: '.html'}))
-        .pipe(gulp.dest(paths.output));
+  return gulp.src(paths.html)
+    .pipe(changed(paths.output, {extension: '.html'}))
+    .pipe(gulp.dest(paths.output));
 });
 // copies changed html files to the output directory
 gulp.task('build-js', function () {
-    return gulp.src(paths.js)
-        // .pipe(changed(paths.output, {extension: '.js'}))
-        .pipe(gulp.dest(paths.jsOutput));
+  return gulp.src(paths.js)
+    .pipe(changed(paths.output, {extension: '.js'}))
+    .pipe(gulp.dest(paths.jsOutput));
 });
 
-
+// this task calls the clean task (located
+// in ./clean.js), then runs the build-system
+// and build-html tasks in parallel
+// https://www.npmjs.com/package/gulp-run-sequence
 gulp.task('build', function (callback) {
-    return runSequence(
-        'clean',
-        ['build-system', 'build-html', 'build-css', 'build-sass', 'build-js'],
-        callback
-    );
+  return runSequence(
+    'clean',
+    ['build-system', 'build-html', 'build-css', 'build-sass','build-js'],
+    callback
+  );
 });
 gulp.task('default', ['build']);
--- build/tasks/watch.js
+++ build/tasks/watch.js
@@ -2,19 +2,14 @@ var gulp = require('gulp');
 var paths = require('../paths');
 var browserSync = require('browser-sync');
 
-// outputs changes to files to the console
-function reportChange(event){
-  console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+function reportChange(event) {
+    console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
 }
 
-// this task wil watch for changes
-// to js, html, and css files and call the
-// reportChange method. Also, by depending on the
-// serve task, it will instantiate a browserSync session
-gulp.task('watch', ['serve'], function() {
+gulp.task('watch', ['serve'], function () {
     gulp.watch(paths.sourceTS, ['build-system', browserSync.reload]).on('change', reportChange);
     gulp.watch(paths.html, ['build-html', browserSync.reload]).on('change', reportChange);
-    gulp.watch(paths.css + '**/*.css', ['build-css',browserSync.reload]).on('change', reportChange);
-    gulp.watch(paths.sass + '**/styles.scss',  ['build-sass',browserSync.reload]).on('change', reportChange);
-    gulp.watch(paths.js, ['build-js',browserSync.reload]).on('change', reportChange);
+    gulp.watch(paths.css, ['build-css', browserSync.reload]).on('change', reportChange);
+    gulp.watch(paths.sass + '**/*.scss', ['build-sass', browserSync.reload]).on('change', reportChange);
+    gulp.watch(paths.js, ['build-js', browserSync.reload]).on('change', reportChange);
 });
